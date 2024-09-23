0b132b656e96deb61b8116218901703e76e625c6
apam <masterchefapam@gmail.com>
2024-09-23 14:02:10 +0800

    home container update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,28 @@
        navClose = document.getElementById('nav-close');
 
 
-if(navToogle){
+if(navToggle){
     navToggle.addEventListener('click',()=>{ 
         navMenu.classList.add('show-menu')
     })
 }
 
 if(navClose){
-    navToggle.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu ')
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
     })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = ()=>{
+   const  navMenu = document.getElementById('nav-menu')
+
+   navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
 
